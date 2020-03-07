41312303900893c7ee6a631dae640ed78734b682
mxzhao <mona.zhao@advent.com>
2020-03-06 21:31:36 -0500

    add pythonbasicknowledge package

---
+++ PythonBasicKnowledge/listdemo.py
@@ -0,0 +1,11 @@
+name="samma zhao"
+print(name.title())
+msg = "i am " + str(29) + " years old"
+print(msg)
+
+
+# 列表
+list = ["i", "am", "samma"]
+print(list[1].title())
+list.sort(reverse=True)
+print(list)
