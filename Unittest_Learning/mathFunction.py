902a2427c6229f092fff904baf333b07fd9078fd
mxzhao <mona.zhao@advent.com>
2020-03-06 22:17:21 -0500

    update package

---
+++ Unittest_Learning/mathFunction.py
@@ -0,0 +1,18 @@
+import sys
+# from unittestDem_1 import *
+# current_working_directory = "C:\\Users\\mxzhao\\PycharmProjects\\UnittestRequestDemo\\unittestDem_1"
+# sys.path.append(current_working_directory)
+import  sys
+def add(a,b):
+    return a+b
+
+def minus(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def divid(a,b):
+    return a/b
+
+divid(4,1)
